90318b259e222a6e0eabcc6e2170bee145214239
Mark Joseph Ronquillo <markronquillo23@gmail.com>
2017-03-08 18:38:14 +0800

    Add react docs advanced example

--- docs/advanced/src/index.js
+++ docs/advanced/src/index.js
@@ -1,9 +1,10 @@
+import 'babel-polyfill'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Root from './containers/Root';
 
-ReactDOM.render(
-  <App />,
+	ReactDOM.render(
+  <Root />,
   document.getElementById('root')
 );
